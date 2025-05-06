63ee00fff37db0a26d62a2137edacaa7d981e3e1
Ishan Bista <mbista313@gmail.com>
2025-05-06 07:51:02 +0545

    quotes added in script.js file

---
+++ script.js
@@ -0,0 +1,19 @@
+const quotes={
+    science:[
+        "Science is a way of thinking much more than it is a body of knowledge. – Carl Sagan",
+    "The important thing is to never stop questioning. – Albert Einstein",
+    "Somewhere, something incredible is waiting to be known. – Carl Sagan"
+
+    ],
+    motivational:[
+        "Don’t watch the clock; do what it does. Keep going. – Sam Levenson",
+        "It always seems impossible until it's done. – Nelson Mandela",
+        "Start where you are. Use what you have. Do what you can. – Arthur Ashe"
+    ],
+    inspirational:[
+        "The only way to do great work is to love what you do.- Steve Jobs",
+        "The future belongs to those who believe in the beauty of their dreams.-Eleanor Roosevelt",
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.-Winston Churchill"
+    ]
+};
+
